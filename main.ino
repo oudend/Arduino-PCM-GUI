93f686aa077cd5f14334abf12c310473b3bda5ac
oudend <ternermartin@gmai.com>
2022-10-24 15:01:47 +0200

    removed missleading comment

--- main.ino
+++ main.ino
@@ -4,7 +4,7 @@
 //computer.h is an example pcm file.
 
 const int SAMPLE_RATE = 10000; // 10 000 sample rate generally isn't recommended for memory efficiency. 2bit, 4k or 8k have produced comparable results.
-const int BITS = 2; // only 1, 2, 4 or 8 supported
+const int BITS = 2;
 
 void setup()
 {
